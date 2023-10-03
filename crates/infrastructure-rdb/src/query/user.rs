879d84fd039ce66632b7fb42939d7fd515a19d9c
vraisamis <quick.sorting.arg@gmail.com>
2023-10-04 08:59:10 +0900

    usersクエリ実装

--- crates/infrastructure-rdb/src/query/user.rs
+++ crates/infrastructure-rdb/src/query/user.rs
@@ -6,6 +6,7 @@ use async_trait::async_trait;
 use domain_kanban::user::UserId;
 use query_resolver::{UserView, UsersQuery};
 use shaku::Provider;
+use sqlx::query_as;
 
 #[derive(Debug, Clone, Provider)]
 #[shaku(interface = UsersQuery)]
@@ -30,15 +31,39 @@ fn to_view(u: sample::User) -> UserView {
 #[async_trait]
 impl UsersQuery for UsersQueryImpl {
     async fn find_by_id(&self, id: &UserId) -> Result<UserView> {
-        let data = sample::data();
-        let result = data
-            .users
-            .clone()
-            .into_iter()
-            .filter(|u| &u.id == id)
-            .map(to_view)
-            .next()
-            .ok_or(anyhow!("Not Found"))?;
+        let pool = self.pool.pool();
+        let executor = pool;
+        let (user_id, name, email): (String, String, String) = query_as(
+            r#"
+            select
+                u.id
+                , u.name
+                , u.email
+            from users u
+            where u.id = ?
+            "#,
+        )
+        .bind(id.to_string())
+        .fetch_one(executor)
+        .await?;
+        let owned_board_ids: Vec<(String,)> = query_as(
+            r#"
+            select board_id
+            from user_board_relations
+            where user_id = ?
+            "#,
+        )
+        .bind(id.to_string())
+        .fetch_all(executor)
+        .await?;
+
+        let result = UserView {
+            id: user_id,
+            name,
+            email,
+            owned_board_ids: owned_board_ids.into_iter().map(|(s,)| s).collect(),
+        };
+
         Ok(result)
     }
     async fn list_by_ids(&self, ids: &[UserId]) -> Result<HashMap<UserId, UserView>> {
