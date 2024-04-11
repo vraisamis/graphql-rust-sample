3ee049b78b3f81568a3fa8adb96ff18368091bd6
vraisamis <quick.sorting.arg@gmail.com>
2024-04-12 07:29:01 +0900

    不変条件チェックの実装例

--- crates/domain-kanban/src/lib.rs
+++ crates/domain-kanban/src/lib.rs
@@ -6,24 +6,38 @@ pub mod user;
 mod todo_invariants {
     use thiserror::Error;
 
-    trait ModelInvariants
+    pub trait ModelInvariants
     where
         Self: 'static + Sized,
     {
         fn invariants() -> Vec<&'static Invariant<Self>>;
-        fn stisfy_invariants(self) -> InvariantResult<Self> {
-            let invariants = Self::invariants();
-            let init: Result<_, InvariantError> = Ok(self);
-            invariants
+        fn stisfy_invariants_ref(&self) -> InvariantResult<&Self> {
+            let init: Result<_, InvariantError> = Ok(());
+            Self::invariants()
                 .iter()
-                .fold(init, |result, func| result.and_then(func))
+                .fold(init, |result, func| result.and_then(|_| func(self)))
+                .map(|_| self)
+        }
+        fn stisfy_invariants_mut(&mut self) -> InvariantResult<&mut Self> {
+            let init: Result<_, InvariantError> = Ok(());
+            Self::invariants()
+                .iter()
+                .fold(init, |result, func| result.and_then(|_| func(&self)))
+                .map(|_| self)
+        }
+        fn stisfy_invariants(self) -> InvariantResult<Self> {
+            let init: Result<_, InvariantError> = Ok(());
+            Self::invariants()
+                .iter()
+                .fold(init, |result, func| result.and_then(|_| func(&self)))
+                .map(|_| self)
         }
     }
 
-    type InvariantResult<T> = Result<T, InvariantError>;
-    type Invariant<T> = dyn Fn(T) -> InvariantResult<T>;
+    pub type InvariantResult<T> = Result<T, InvariantError>;
+    pub type Invariant<T> = dyn Fn(&T) -> InvariantResult<()>;
 
-    #[derive(Debug, Error)]
+    #[derive(Debug, Error, Clone, PartialEq, Eq)]
     pub enum InvariantError {
         #[error("不変条件違反: {0}")]
         ViolationError(String),
--- crates/domain-kanban/src/user.rs
+++ crates/domain-kanban/src/user.rs
@@ -1,5 +1,7 @@
 use domain_util::{Entity, Identifier};
 
+use crate::todo_invariants::{Invariant, InvariantError, InvariantResult, ModelInvariants};
+
 #[allow(unused)]
 #[derive(Debug)]
 pub struct User {
@@ -9,6 +11,12 @@ pub struct User {
 }
 
 impl User {
+    pub fn new_check(
+        name: InvariantResult<UserName>,
+        email: InvariantResult<Email>,
+    ) -> InvariantResult<Self> {
+        Self::new(name?, email?).stisfy_invariants()
+    }
     pub fn new(name: UserName, email: Email) -> Self {
         let user_id = UserId::gen();
         Self {
@@ -23,6 +31,12 @@ impl User {
     }
 }
 
+impl ModelInvariants for User {
+    fn invariants() -> Vec<&'static Invariant<Self>> {
+        vec![]
+    }
+}
+
 impl Entity for User {
     fn entity_type() -> &'static str {
         "user"
@@ -31,8 +45,17 @@ impl Entity for User {
 
 pub type UserId = Identifier<User>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct UserName(String);
+
+impl UserName {
+    pub fn new(value: String) -> InvariantResult<Self> {
+        Self::new_unchecked(value).stisfy_invariants()
+    }
+    pub fn new_unchecked(value: String) -> Self {
+        Self(value)
+    }
+}
 
 impl From<String> for UserName {
     fn from(value: String) -> Self {
@@ -40,12 +63,53 @@ impl From<String> for UserName {
     }
 }
 
-#[derive(Debug)]
+fn user_name_length_less_than_20(name: &UserName) -> InvariantResult<()> {
+    if name.0.len() <= 20 {
+        Ok(())
+    } else {
+        Err(InvariantError::ViolationError(
+            "名前が長すぎます。20文字以内にしてください".to_owned(),
+        ))
+    }
+}
+
+impl ModelInvariants for UserName {
+    fn invariants() -> Vec<&'static Invariant<Self>> {
+        vec![&user_name_length_less_than_20]
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Email(String);
+
+impl Email {
+    pub fn new(value: String) -> InvariantResult<Self> {
+        Self::new_unchecked(value).stisfy_invariants()
+    }
+    pub fn new_unchecked(value: String) -> Self {
+        Self(value)
+    }
+}
 
 impl From<String> for Email {
     fn from(value: String) -> Self {
         Self(value)
+    }
+}
+
+fn email_contains_atmark(email: &Email) -> InvariantResult<()> {
+    if email.0.contains("@") {
+        Ok(())
+    } else {
+        Err(InvariantError::ViolationError(
+            "メールアドレスに「@」が含まれていません".to_string(),
+        ))
+    }
+}
+
+impl ModelInvariants for Email {
+    fn invariants() -> Vec<&'static Invariant<Self>> {
+        vec![&email_contains_atmark]
     }
 }
 
@@ -61,5 +125,34 @@ mod tests {
         let user_id = user.user_id();
 
         println!("{:?}, id: {:?}", user, user_id);
+    }
+
+    #[test]
+    fn email_ok() {
+        let email = Email::from("hoge@example.com".to_owned());
+        let result = email.clone().stisfy_invariants();
+        assert_eq!(result, Ok(email.clone()));
+        let result = email.stisfy_invariants_ref();
+        assert_eq!(result, Ok(&email));
+    }
+
+    #[test]
+    fn email_ng() {
+        let email = Email::from("hoge_example.com".to_owned());
+        let result = email.clone().stisfy_invariants();
+        assert_eq!(
+            result,
+            Err(InvariantError::ViolationError(
+                "メールアドレスに「@」が含まれていません".to_owned()
+            ))
+        );
+
+        let result = email.stisfy_invariants_ref();
+        assert_eq!(
+            result,
+            Err(InvariantError::ViolationError(
+                "メールアドレスに「@」が含まれていません".to_owned()
+            ))
+        );
     }
 }
